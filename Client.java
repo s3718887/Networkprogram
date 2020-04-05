14189a5e52ac20a4dbeb6eb7849e536c4518bbd3
Jung <s3718887@student.rmit.edu.au>
2020-04-05 19:39:44 +1000

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client
+{
+    private final static String ADDRESS = "netprog1.csit.rmit.edu.au ";
+    private final static int PORT = 61141;
+
+    private Socket connection;
+
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    public Client(String fileName)
+    {
+        try
+        {
+            connection = new Socket(ADDRESS, PORT);
+
+            outputStream = connection.getOutputStream();
+            inputStream = connection.getInputStream();
+
+            send(fileName);
+            receive(fileName + "_out." + fileName.split("\\.")[1]);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void send(String fileName)
+    {
+        try
+        {
+            outputStream.write((fileName).getBytes());
+            outputStream.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void receive(String fileName)
+    {
+        byte [] buffer = new byte[1024];
+        int bytesRead;
+
+        File file = new File(fileName);
+
+        try
+        {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+            /* Reads and write the byte stream from the client onto the server disk */
+            do {
+                bytesRead = inputStream.read(buffer);
+
+                if (bytesRead <= 0)
+                    break;
+
+                if(new String(buffer).startsWith("Error") )
+                {
+                    System.out.println(new String(buffer).trim());
+                    break;
+                }
+
+                bufferedOutputStream.write(buffer, 0, bytesRead);
+                bufferedOutputStream.flush();
+            }
+            while (true);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        new Client(args[0]);
+    }
+}
---
+++ Server.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    private final static int PORT = 61141 ;
+
+    private Socket connection;
+
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    private String request;
+
+    public Server()
+    {
+        ServerSocket server = null;
+
+        try
+        {
+            server = new ServerSocket(PORT);
+            System.out.println("Starts the server");
+
+            while (true)
+            {
+                System.out.println("Waiting for new connections...");
+                connection = server.accept();
+                System.out.println("Accepted");
+
+                outputStream = connection.getOutputStream();
+                outputStream.flush();
+                inputStream = connection.getInputStream();
+
+
+                request = readFromClient(inputStream);
+                System.out.println("Request: " + request);
+                byte [] buffer = readFromDisk(request);
+                writeToClient(buffer);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                server.close();
+            }
+            catch (IOException e) { }
+        }
+    }
+
+    private String readFromClient(InputStream inputStream)
+    {
+        int character = 0;
+        StringBuffer stringBuffer = new StringBuffer();
+        byte [] buffer = new byte[1024];
+
+        try
+        {
+            inputStream.read(buffer);
+
+            for(int i=0; i < 1024; i++) {
+                if(buffer[i] == 0)
+                    break;
+
+                stringBuffer.append((char)buffer[i]);
+            }
+            System.out.println("finished reading");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return stringBuffer.toString();
+    }
+
+    private byte [] readFromDisk(String request)
+    {
+        File file = new File(request);
+        BufferedInputStream bufferedInputStream = null;
+
+        byte [] buffer = new byte[(int)file.length()];
+
+        try
+        {
+            FileInputStream fileInputStream = new FileInputStream(request);
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            bufferedInputStream.read(buffer,0 , buffer.length);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Error, file not found : " + request);
+            buffer = "Error, Server couldn't find your file".getBytes();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (bufferedInputStream != null) bufferedInputStream.close();
+            }
+            catch (IOException e)
+            {
+
+            }
+        }
+
+        return buffer;
+    }
+
+    private void writeToClient(byte [] buffer)
+    {
+        try
+        {
+            outputStream.write(buffer); // Writes the complete data to the server
+            outputStream.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                if(outputStream != null) outputStream.close();
+                if(inputStream != null) inputStream.close();
+                if(connection != null) connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        new Server();
+    }
+}
